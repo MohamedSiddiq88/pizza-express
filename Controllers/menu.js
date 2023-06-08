b9186494432732e44660b403f42d86d894b971c1
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-06-09 02:09:09 +0530

    new

--- Controllers/menu.js
+++ Controllers/menu.js
@@ -5,7 +5,8 @@ export async function getCustomizationOptions() {
   return client
       .db("restaurant")
       .collection("PizzaCustomizationOptions")
-      .find({});
+      .find()
+      .toArray();
       
 }
 
