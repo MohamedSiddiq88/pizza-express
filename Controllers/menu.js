0ab8786f72d889b90e6034ab87b69cdb83e3796d
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-06-15 19:15:36 +0530

    new

--- Controllers/menu.js
+++ Controllers/menu.js
@@ -10,64 +10,97 @@ export async function getCustomizationOptions() {
       
 }
 
-export async function getCustomizationOptionByName(name) {
-  const options = await client
-    .db("restaurant")
-    .collection("PizzaCustomizationOptions")
-    .find()
-    .toArray();
+export function updateCustomizationOptionByName(
+  name,
+  updatedCustomizationOption
+) {
+  const filter = {};
+  const update = {};
 
-  for (const option of options) {
-    if (option.pizzaBase) {
-      for (const base of option.pizzaBase) {
-        if (base.name === name) {
-          return base;
-        }
-      }
-    }
-    if (option.sauce) {
-      for (const sauce of option.sauce) {
-        if (sauce.name === name) {
-          return sauce;
-        }
-      }
-    }
-    if (option.cheese) {
-      for (const cheese of option.cheese) {
-        if (cheese.name === name) {
-          return cheese;
-        }
-      }
-    }
-    if (option.veggies) {
-      for (const veggie of option.veggies) {
-        if (veggie.name === name) {
-          return veggie;
-        }
-      }
-    }
-    if (option.meat) {
-      for (const meat of option.meat) {
-        if (meat.name === name) {
-          return meat;
-        }
-      }
-    }
+  if (updatedCustomizationOption.pizzaBase) {
+    filter["pizzaBase.name"] = name;
+    update.$set = { "pizzaBase.$": updatedCustomizationOption };
+  } else if (updatedCustomizationOption.sauce) {
+    filter["sauce.name"] = name;
+    update.$set = { "sauce.$": updatedCustomizationOption };
+  } else if (updatedCustomizationOption.cheese) {
+    filter["cheese.name"] = name;
+    update.$set = { "cheese.$": updatedCustomizationOption };
+  } else if (updatedCustomizationOption.veggies) {
+    filter["veggies.name"] = name;
+    update.$set = { "veggies.$": updatedCustomizationOption };
+  } else if (updatedCustomizationOption.meat) {
+    filter["meat.name"] = name;
+    update.$set = { "meat.$": updatedCustomizationOption };
+  } else {
+    return null; // Invalid customization option
   }
 
-  return null; // Customization option not found
+  return client
+    .db("restaurant")
+    .collection("PizzaCustomizationOptions")
+    .findOneAndUpdate(filter, update);
 }
 
 
-// export function updateCustomizationOptionByName(name, updatedCustomizationOption) {
-//   return client
+// export async function getCustomizationOptionByName(name) {
+//   const options = await client
 //     .db("restaurant")
 //     .collection("PizzaCustomizationOptions")
-//     .findOneAndUpdate(
-//       { name: name },
-//       { $set: updatedCustomizationOption }
-//     );
+//     .find()
+//     .toArray();
+
+//   for (const option of options) {
+//     if (option.pizzaBase) {
+//       for (const base of option.pizzaBase) {
+//         if (base.name === name) {
+//           return base;
+//         }
+//       }
+//     }
+//     if (option.sauce) {
+//       for (const sauce of option.sauce) {
+//         if (sauce.name === name) {
+//           return sauce;
+//         }
+//       }
+//     }
+//     if (option.cheese) {
+//       for (const cheese of option.cheese) {
+//         if (cheese.name === name) {
+//           return cheese;
+//         }
+//       }
+//     }
+//     if (option.veggies) {
+//       for (const veggie of option.veggies) {
+//         if (veggie.name === name) {
+//           return veggie;
+//         }
+//       }
+//     }
+//     if (option.meat) {
+//       for (const meat of option.meat) {
+//         if (meat.name === name) {
+//           return meat;
+//         }
+//       }
+//     }
+//   }
+
+//   return null; // Customization option not found
 // }
+
+
+export function updateCustomizationOptionByName(name, updatedCustomizationOption) {
+  return client
+    .db("restaurant")
+    .collection("PizzaCustomizationOptions")
+    .findOneAndUpdate(
+      { name: name },
+      { $set: updatedCustomizationOption }
+    );
+}
 
 
 // export function updateMenuByPizzaBaseName(name, updatedQuantity) {
